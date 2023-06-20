cb194516246aa4ce5dcc5461108924925239b841
islamerlanuulu <islamerlanuulu3939@gmail.com>
2023-06-20 08:06:54 +0600

    Initial Commit

---
+++ homeTask-1.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeTask-1.5
+//
+//  Created by Islam Erlan Uulu on 20/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
